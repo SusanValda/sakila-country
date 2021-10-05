531f9c875bc150a80eaf9819f4d13d06b6df9092
isuuki <susan.valda@ucb.edu.bo>
2021-10-04 20:05:09 -0400

    Initial commit

---
+++ src/main/java/bo/edu/ucb/ingsoft/sakilacountry/SakilaCountryApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.ingsoft.sakilacountry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakilaCountryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakilaCountryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/edu/ucb/ingsoft/sakilacountry/SakilaCountryApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.ingsoft.sakilacountry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakilaCountryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
